026a066c58cd2d7bb37d9500754fff40d1db1620
carolmedici <carolina.obstetriz@gmail.com>
2023-05-18 22:59:48 -0300

    :sparkles: counter adding and subtracting

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <h1>Score Count</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          + {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}> + </button>    {count}
+               <button onClick={() => setCount((count) => count - 1)}>-
         
-    </>
+          
+        </button>
+
+        
+    </div>
   )
 }
 
